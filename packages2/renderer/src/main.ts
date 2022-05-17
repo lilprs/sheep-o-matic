cf32705741f1780dc454e177802c82149e0364c3
Lilian Prus <h3ruta@icloud.com>
2022-05-17 19:19:41 +0200

    rm old code

--- packages2/renderer/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './style.scss'
-import 'v-calendar/dist/style.css'
-
-createApp(App).mount('#app').$nextTick(window.removeLoading)
-
-console.log('fs', window.fs)
-console.log('ipcRenderer', window.ipcRenderer)
